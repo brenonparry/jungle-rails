c2fbce33a8cc7c6ddda11441462beb02df9ae760
Brenon <brenonparry@gmail.com>
2022-02-08 20:58:16 +0000

    added an initial example that ensures that a product with all four fields set will indeed save successfully

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,6 +3,17 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   describe 'Validations' do
-    # validation tests/examples here
+    
+   it "will save new product when all feild are set correclty" do
+
+    @category = Category.new(name: "Furniture")
+    @product = Product.new(name: "Divorced-Dad Futon", price: 199, quantity: 12, category: @category)
+
+    @product.save 
+    expect(Product.find_by(name: "Divorced-Dad Futon")).not_to be_nil
+
+   end
+
+   
   end
 end
