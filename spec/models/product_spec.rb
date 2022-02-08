53b144393e341a6f61566b8f29a101f021fe3ddd
Brenon <brenonparry@gmail.com>
2022-02-08 21:17:13 +0000

    Added 4 more tests to check name, price, qunitity and category validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  
   describe 'Validations' do
     
    it "will save new product when all feild are set correclty" do
@@ -14,6 +14,53 @@ RSpec.describe Product, type: :model do
 
    end
 
-   
+   it "will not save new product when name feild is not set correclty" do
+
+    @category = Category.new(name: "Furniture")
+    @product = Product.new(name: "Divorced-Dad Futon", price: 199, quantity: 12, category: @category)
+
+    @product.name = nil
+    @product.save 
+    # puts "++++++++++#{@product.errors.full_messages}"
+    expect(@product.errors.full_messages).to include("Name can't be blank")
+
+   end
+
+   it "will not save new product when price feild is not set correclty" do
+
+    @category = Category.new(name: "Furniture")
+    @product = Product.new(name: "Divorced-Dad Futon", price: 199, quantity: 12, category: @category)
+
+    @product.price_cents = nil
+    @product.save 
+    # puts "++++++++++#{@product.errors.full_messages}"
+    expect(@product.errors.full_messages).to include("Price is not a number")
+
+   end
+
+   it "will not save new product when quantity feild is not set correclty" do
+
+    @category = Category.new(name: "Furniture")
+    @product = Product.new(name: "Divorced-Dad Futon", price: 199, quantity: 12, category: @category)
+
+    @product.quantity = nil
+    @product.save 
+    # puts "++++++++++#{@product.errors.full_messages}"
+    expect(@product.errors.full_messages).to include("Quantity can't be blank")
+
+   end
+
+   it "will not save new product when category feild is not set correclty" do
+
+    @category = Category.new(name: "Furniture")
+    @product = Product.new(name: "Divorced-Dad Futon", price: 199, quantity: 12, category: @category)
+
+    @product.category = nil
+    @product.save 
+    # puts "++++++++++#{@product.errors.full_messages}"
+    expect(@product.errors.full_messages).to include("Category can't be blank")
+
+   end
+
   end
 end
